c9eb5d24f9e8b909d437c8d05fdefb2280529f74
Сергей Пузырев <sergii.puzyrov@gmail.com>
2026-03-11 13:46:59 +0200

    Added function example

---
+++ apps/func_example/include/app.hpp
@@ -0,0 +1,138 @@
+﻿#pragma once
+
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <string>
+
+struct Employee {
+	int id;
+	double salary;
+	std::string name;
+};
+
+bool operator<(const Employee e1, const Employee e2)
+{
+	//return e1.salary < e2.salary;
+	return e1.name.size() < e2.name.size();
+}
+
+bool operator>(const Employee e1, const Employee e2)
+{
+	//return e1.salary > e2.salary;
+	return e1.name.size() > e2.name.size();
+}
+
+
+std::ostream& operator<<(std::ostream& os, const Employee& e)
+{
+	os << "Employee {";
+	os << "id: " << e.id;
+	os << ", name: " << e.name;
+	os << ", salary: " << e.salary;
+	os << "}";
+	return os;
+}
+
+template<typename T>
+T min(T* a, const int& sz)
+{
+	T res = a[0];
+
+	for (int i = 1; i < sz; ++i)
+	{
+		if (a[i] < res)
+		{
+			res = a[i];
+		}
+	}
+
+	return res;
+}
+
+template<typename T, int sz>
+T min(T (&a)[sz])
+{
+	T res = a[0];
+
+	for (int i = 1; i < sz; ++i)
+	{
+		if (a[i] < res)
+		{
+			res = a[i];
+		}
+	}
+
+	return res;
+}
+
+
+template<typename T>
+T max(T* a, const int& sz)
+{
+	T res = a[0];
+
+	for (int i = 1; i < sz; ++i)
+	{
+		if (a[i] > res)
+		{
+			res = a[i];
+		}
+	}
+
+	return res;
+}
+
+template<typename T, int sz>
+T max(T (&a)[sz])
+{
+	T res = a[0];
+
+	for (int i = 1; i < sz; ++i)
+	{
+		if (a[i] > res)
+		{
+			res = a[i];
+		}
+	}
+
+	return res;
+}
+
+template<typename T>
+void min_max(T* a, const int& sz, T& min, T& max)
+{
+	min = a[0], max = a[0];
+
+	for (int i = 1; i < sz; ++i)
+	{
+		if (a[i] < min)
+		{
+			min = a[i];
+		}
+
+		if (a[i] > max)
+		{
+			max = a[i];
+		}
+	}
+}
+
+template<typename T, int sz>
+void min_max(T (&a)[sz], T& min, T& max)
+{
+	min = a[0], max = a[0];
+
+	for (int i = 1; i < sz; ++i)
+	{
+		if (a[i] < min)
+		{
+			min = a[i];
+		}
+
+		if (a[i] > max)
+		{
+			max = a[i];
+		}
+	}
+}
---
+++ apps/func_example/src/main.cpp
@@ -0,0 +1,48 @@
+﻿#include "app.hpp"
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::setprecision;
+using std::fixed;
+using std::setw;
+
+int main() {
+    
+    int a1[] = { 1, -3, 5, 8, -4 };
+
+    int sz = sizeof(a1) / sizeof(a1[0]);
+
+    auto min1 = min(a1, sz);
+
+
+    cout << "Min: " << min1 << endl;
+
+
+    {
+        int min1, max1;
+
+        min_max(a1, sz, min1, max1);
+
+        cout << "Min: " << min1  << "\tMax: " << max1 << endl;
+
+    }
+
+    {
+
+        Employee employees[] = {
+            {1, 12901.87, "John Doe"},
+            {2, 47791.68, "Sam Altman"},
+            {3, 438891424.349, "Bill W. Geits"},
+        };
+
+        Employee min1, max1;
+
+        min_max(employees, min1, max1);
+
+        cout << min1 << endl;
+        cout << max1 << endl;
+    }
+
+    return 0;
+}
